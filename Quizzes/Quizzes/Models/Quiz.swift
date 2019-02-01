fc83859875082be21a2560af5be1cad12a6057be
natmess <nnmessier@gmail.com>
2019-02-01 17:10:57 -0500

    created model for Quiz and API Client, received data from API

---
+++ Quizzes/Quizzes/Models/Quiz.swift
@@ -0,0 +1,13 @@
+//
+//  Quiz.swift
+//  Quizzes
+//
+//  Created by Nathalie  on 2/1/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+struct Quiz {
+    
+}
---
+++ Quizzes/Quizzes/Network Services/QuizzesAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  QuizzesAPIClient.swift
+//  Quizzes
+//
+//  Created by Nathalie  on 2/1/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
